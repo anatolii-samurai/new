285db915980f887155aded7d335cfc6e0a60d5b8
ivan <example@gmail.com>
2022-08-22 20:22:18 +0400

    add script.js

--- jsLesson.js
+++ jsLesson.js
@@ -1,67 +1,18 @@
 "use strict";
 
-// let result = '';
-// const length = 7;
+console.log(4 + +"5");
 
-// for(let i = 0; i < length; i++){
+let incr = 10,
+    decr = 10;
 
-//     for(let j = 0; j < i; j++){
-//         result+="*";
-//     }
+    // ++incr;
+    // --decr;
+console.log(++incr);
+console.log(--decr);
 
-//     result+='\n';
-// }
-// console.log(result);
+console.log(5%2);
 
+const isChecked = true,
+    isClose = false;
 
-//     // Пишем решение вот тут
-// for (let i = 5; i<=10; i++){
-//     console.log(i)
-// } 
-// for (let i = 20; i>=10; i--){
-//     if (i===13){
-//         break;
-//     }
-//     console.log(i);}
-//     for (let i = 2; i <= 10; i++){
-//         if(i%2==0){console.log (i);}
-        
-//     }    
-    // for (let i = 2; i <= 16; i++) {
-    //     if (i % 2 === 0) {
-    //         continue;
-    //     } else {
-    //         console.log(i);
-    //     }
-    // }  
-
-//     let num = 2;
-
-//     while (num<=16){
-//         num ++;
-//         if(num%2===0){
-//             console.log(num);
-//         }
-//     }
-    
-    // for (let i = 2; i <= 16; i++) {
-    //     if (i % 2 === 0) {
-    //         continue;
-    //     } else {
-    //         console.log(i);
-    //     }
-    // }
-
-// const arrayOfNumbers = [];
-// for (let i = 5; i < 10; i++) {
-//     arrayOfNumbers[i-5]=i;
-// }
-// console.log(arrayOfNumbers);
-
-// Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [...arr];
-// Пишем решение вот тут
-console.log(result);
-// Не трогаем
-return result;
+console.log(isChecked || isClose);
